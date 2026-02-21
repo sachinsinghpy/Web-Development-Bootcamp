89587665abf59342f8a5760f7b866e7aca2cc771
Sachin <skraja012345678@gmail.com>
2026-02-21 23:38:36 +0530

    initial commit

---
+++ JavaScript Basic/String.js
@@ -0,0 +1,12 @@
+let students = "Sachin";
+for (const element of students) {
+    console.log(element);
+    
+}
+
+let name = "Bittu";
+let names = `Twinkle Twinkle Little Star 
+                            -By Sachin`
+
+console.log(names)
+
